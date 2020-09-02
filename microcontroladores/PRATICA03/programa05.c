0a88ae06664e9c6af78a8acf981b1b403e5f3c76
gihonorato <69973659+gihonorato@users.noreply.github.com>
2020-09-02 10:10:19 -0300

    Create programa05.c

---
+++ microcontroladores/PRATICA03/programa05.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+
+int main(void){
+int minhaVariavel;
+minhaVariavel = 10;
+printf("Valor da minhaVariavel: %i\n", minhaVariavel); //Apresenta o conteúdo
+printf("Valor da minhaVariavel: %i\n", &minhaVariavel); //Apresenta o endereço
+return 0;
+}
