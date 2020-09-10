3ac46c369b6a067192055c0dd431812800f05d2e
gihonorato <69973659+gihonorato@users.noreply.github.com>
2020-09-10 10:32:44 -0300

    Delete programa03.py

--- PRATICA 02/programa03.py
+++
@@ -1,6 +0,0 @@
-prog. 3:
-#Usuário fala dois números e nós calculamos a média deles
-numero1 = int(input("Numero 1:"))
-numero2 = int(input("Numero 2:"))
-media = (numero1 + numero2) / 2
-print("o resultado da media:", media)
